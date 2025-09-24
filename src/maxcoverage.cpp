5dfeee6670006915497c27dc015b7f0bad7b90f2
Enno Adler <eadler@mail.uni-paderborn.de>
2025-09-24 17:23:00 +0200

    #2 - fixed SA-interval start

--- src/maxcoverage.cpp
+++ src/maxcoverage.cpp
@@ -10,14 +10,14 @@
 using namespace std;
 using namespace sdsl;
 
-uint64_t compute_overlap(int_vector<> sa, int_vector<> rst, size_t pos, uint64_t lcp)
+uint64_t compute_overlap(int_vector<> sa, int_vector<> rst, int_vector<> q1, size_t pos, uint64_t lcp)
 {
     // Ensure we don't go out of bounds
     if (pos + rst[pos] > sa.size())
         rst[pos] = sa.size() - pos;
 
     // Copy range [pos, pos+count)
-    std::vector<size_t> subset(sa.begin() + pos - 1, sa.begin() + pos + rst[pos] - 1);
+    std::vector<size_t> subset(sa.begin() + q1[pos] - 1, sa.begin() + q1[pos] + rst[pos] - 1);
 
     // Sort the values
     std::sort(subset.begin(), subset.end());
@@ -35,6 +35,7 @@ uint64_t compute_overlap(int_vector<> sa, int_vector<> rst, size_t pos, uint64_t
 
 string compute_max_coverage(const string& test_string)
 {
+    cout << "Computing real substring of " << test_string << " that covers the most positions." << endl;
     //int_vector<> sa(test_string.size(), 0, bits::hi(test_string.size())+1);
     //algorithm::calculate_sa((const unsigned char*)test_string.data(), test_string.size(), sa);
     csa_bitcompressed sa;
@@ -52,35 +53,41 @@ string compute_max_coverage(const string& test_string)
     cout << lcp << endl;  // output LCP
 
     int_vector<> rst(sa.size(), 0);
+    int_vector<> q1(sa.size(), 0);
 
-    std::stack<std::pair<size_t, uint64_t>> lcp_stack;
+    // TODO: Wrong, as it not considers the equal prefixes before the exact match.
+    // Stack entry: <<block begin Q1, position of max block height k>, lcp[k]>.
+    std::stack<std::pair<std::pair<size_t, size_t>, uint64_t>> lcp_stack;
     // Fill stack with pairs
     for (size_t i = 0; i < lcp.size(); i++) {
         bool condition = true;
-        std::pair<size_t, uint64_t> top;
+        size_t interval_start = i;
+        std::pair<std::pair<size_t, size_t>, uint64_t> top;
         while (!lcp_stack.empty() and condition) {
             top = lcp_stack.top();
             if (top.second < lcp[i]){
-                lcp_stack.push({i, lcp[i]});
+                lcp_stack.push({{interval_start, i}, lcp[i]});
                 condition = false;
             }
             if (top.second > lcp[i]) {
-                rst[top.first] = i - top.first + 1;
+                rst[top.first.second] = i - top.first.first + 1;
+                q1[top.first.second] = top.first.first;
+                interval_start = top.first.first; // Before the block where higher lcp-blocks we need to count
                 lcp_stack.pop();
             }
             if (top.second == lcp[i])
                 condition = false;
         }
         if (lcp_stack.empty()) {
-            lcp_stack.push({i, lcp[i]});
+            lcp_stack.push({{interval_start, i}, lcp[i]});
         }
     }
     //Postprocess Stack
     while (!lcp_stack.empty())
     {
-        std::pair<size_t, uint64_t> top;
+        std::pair<std::pair<size_t, size_t>, uint64_t> top;
         top = lcp_stack.top();
-        rst[top.first] = lcp.size() - top.first + 1;
+        rst[top.first.second] = lcp.size() - top.first.first + 1;
         lcp_stack.pop();
     }
 
@@ -106,7 +113,7 @@ string compute_max_coverage(const string& test_string)
     if (lcp_heap.empty())
         throw -1;
     auto [max_coverage, max_cov_pos] = lcp_heap.top();
-    max_coverage -= compute_overlap(sa.sa_sample, rst, max_cov_pos, lcp[max_cov_pos]);
+    max_coverage -= compute_overlap(sa.sa_sample, rst, q1, max_cov_pos, lcp[max_cov_pos]);
 
     cout << "Heap nodes (LCP[i] * RST[i], i):" << endl;
     // Process Heap
@@ -115,11 +122,11 @@ string compute_max_coverage(const string& test_string)
         std::cout << "(" << val << ", " << pos << ")\n";
         if (val < max_coverage)
             break;
-        max_coverage = val - compute_overlap(sa.sa_sample, rst, max_cov_pos, lcp[max_cov_pos]);
+        max_coverage = val - compute_overlap(sa.sa_sample, rst, q1, max_cov_pos, lcp[max_cov_pos]);
         max_cov_pos = pos;
         lcp_heap.pop();
     }
 
-    cout << "Result: " << test_string.substr(sa[max_cov_pos], sa[max_cov_pos]+lcp[max_cov_pos]) << " covers " << max_coverage << endl;
-    return test_string.substr(sa[max_cov_pos], sa[max_cov_pos]+lcp[max_cov_pos]);
+    cout << "Result: " << test_string.substr(sa[max_cov_pos], lcp[max_cov_pos]) << " covers " << max_coverage << endl;
+    return test_string.substr(sa[max_cov_pos], lcp[max_cov_pos]);
 }
